59df12f9d1d35b3fa67eb58372b1572a00fa85a1
Aztarok <Aztarok2994@gmail.com>
2024-03-31 12:44:38 -0400

    small fix

--- src/components/Posts/FetchMore.tsx
+++ src/components/Posts/FetchMore.tsx
@@ -38,11 +38,8 @@ const FetchMore = ({ FollowingList }: { FollowingList?: any }) => {
 	const scrollPercentage = useRef<number>(0);
 	const [fetching, setFetching] = useState(false);
 	const [turnOff, setTurnOff] = useState<boolean>(false);
-	const [user, setUser] = useState<any>(async () => {
-		return await supabase.auth.getUser();
-	});
 	const getFromAndTo = () => {
-		const ITEMS_PER_PAGE = 20;
+		const ITEMS_PER_PAGE = 5;
 		let from = page * ITEMS_PER_PAGE;
 		let to = from + ITEMS_PER_PAGE;
 
@@ -87,44 +84,50 @@ const FetchMore = ({ FollowingList }: { FollowingList?: any }) => {
 	const re = /(?:\.([^.]+))?$/;
 	let imagePrio = false;
 
-	useEffect(() => {
-		if (!wasAlreadyReqested.current) {
-			getPosts();
+	const handleScroll = debounce(() => {
+		const scrollTop =
+			window.scrollY !== undefined
+				? window.scrollY
+				: (
+						document.documentElement ||
+						document.body.parentNode ||
+						document.body
+				  ).scrollTop;
+		const scrollHeight =
+			document.documentElement.scrollHeight || document.body.scrollHeight;
+		const clientHeight =
+			document.documentElement.clientHeight || document.body.clientHeight;
+		const newScrollPercentage =
+			(scrollTop / (scrollHeight - clientHeight)) * 100;
+		scrollPercentage.current = newScrollPercentage;
+
+		// Fetch more posts when scroll reaches 98%
+		if (
+			scrollPercentage.current.valueOf() > 98 &&
+			!fetching &&
+			!turnOff &&
+			tabDisplay === 'For you'
+		) {
+			setFetching(true);
+			document.getElementById('fetch')?.click();
 		}
-		wasAlreadyReqested.current = true;
+		if (
+			scrollPercentage.current.valueOf() > 98 &&
+			!fetching &&
+			!turnOff &&
+			tabDisplay === 'Following'
+		) {
+			setFetching(true);
+			runFollowing();
+		}
+	}, 1000);
+	const handleResize = () => {
+		handleScroll();
+	};
 
-		const handleScroll = debounce(() => {
-			const scrollTop =
-				window.scrollY !== undefined
-					? window.scrollY
-					: (
-							document.documentElement ||
-							document.body.parentNode ||
-							document.body
-					  ).scrollTop;
-			const scrollHeight =
-				document.documentElement.scrollHeight ||
-				document.body.scrollHeight;
-			const clientHeight =
-				document.documentElement.clientHeight ||
-				document.body.clientHeight;
-			const newScrollPercentage =
-				(scrollTop / (scrollHeight - clientHeight)) * 100;
-			scrollPercentage.current = newScrollPercentage;
+	useEffect(() => {
+		console.log('hi');
 
-			// Fetch more posts when scroll reaches 98%
-			if (
-				scrollPercentage.current.valueOf() > 98 &&
-				!fetching &&
-				!turnOff
-			) {
-				setFetching(true);
-				document.getElementById('fetch')?.click();
-			}
-		}, 1000);
-		const handleResize = () => {
-			handleScroll();
-		};
 		window.addEventListener('scroll', handleScroll);
 		window.addEventListener('resize', handleResize);
 
@@ -137,27 +140,7 @@ const FetchMore = ({ FollowingList }: { FollowingList?: any }) => {
 			window.removeEventListener('resize', handleResize);
 		};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [wasAlreadyReqested, fetching, scrollPercentage]);
-
-	const runFollowing = async () => {
-		let idsForFollowing = FollowingList?.map(
-			(item: any) => item.following_id
-		);
-		const { data } = await supabase
-			.from('posts')
-			.select(
-				'*,images(name),profiles(display_name, handle, image_url, role)'
-			)
-			.in('post_by', idsForFollowing)
-			.order('created_at', { ascending: false });
-		if (!data) {
-			toast.error('Failed to get posts');
-			return;
-		}
-		const formattedPosts = data.map(createPostObject);
-		setPosts((currentPosts: any) => [...currentPosts, ...formattedPosts]);
-		setFetching(false);
-	};
+	}, [scrollPercentage, tabDisplay]);
 
 	useEffect(() => {
 		if (tabDisplay === 'Following') {
@@ -168,12 +151,35 @@ const FetchMore = ({ FollowingList }: { FollowingList?: any }) => {
 		}
 		if (tabDisplay === 'For you') {
 			setPosts([]);
+			setPage(0);
 			getPosts();
 			setFetching(true);
 		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [tabDisplay]);
 
+	const runFollowing = async () => {
+		let idsForFollowing = FollowingList?.map(
+			(item: any) => item.following_id
+		);
+		console.log(page);
+		const { from, to } = getFromAndTo();
+		const { data } = await supabase
+			.from('posts')
+			.select(
+				'*,images(name),profiles(display_name, handle, image_url, role)'
+			)
+			.in('post_by', idsForFollowing)
+			.range(from, to)
+			.order('created_at', { ascending: false });
+		if (!data) {
+			toast.error('Failed to get posts');
+			return;
+		}
+		setPage(page + 1);
+		const formattedPosts = data.map(createPostObject);
+		setPosts((currentPosts: any) => [...currentPosts, ...formattedPosts]);
+		setFetching(false);
+	};
 	const getTest = async () => {
 		console.log('hi');
 		console.log(tabDisplay);
